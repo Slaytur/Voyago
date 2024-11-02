8ccddf137d24885fc013458561caffcdedd3e4dc
Damien Vesper <ldamienvesper@gmail.com>
2024-11-02 06:45:07 -0500

    fix: git fuckup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,61 @@
-import { sveltekit } from "@sveltejs/kit/vite";
+import { version } from "./package.json";
+
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-    plugins: [sveltekit()],
-    test: {
-        include: ["src/**/*.{test,spec}.{js,ts}"]
-    },
+import { sveltekit } from "@sveltejs/kit/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-    server: {
-        port: 3000
-    }
+export default defineConfig(({ mode }) => {
+    /**
+     * Vite environment variables.
+     */
+    process.env = {
+        ...process.env,
+        VITE_APP_VERSION: version
+    };
+
+    return {
+        build: {
+            chunkSizeWarningLimit: 8000
+        },
+
+        test: {
+            include: ["src/**/*.{test,spec}.{js,ts}"]
+        },
+
+        server: {
+            port: 3000,
+            host: "0.0.0.0"
+        },
+
+        preview: {
+            port: 3000,
+            host: "0.0.0.0"
+        },
+
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler"
+                }
+            },
+            devSourcemap: mode === "development"
+        },
+
+        plugins: [
+            sveltekit(),
+            ViteImageOptimizer({
+                test: /\.(svg)$/i,
+                logStats: false
+            })
+        ],
+
+        define: {
+            APP_VERSION: JSON.stringify(version),
+            API_URL: JSON.stringify(mode === "production"
+                ? "/api"
+                : "http://127.0.0.1:8080"
+            )
+        }
+    };
 });
