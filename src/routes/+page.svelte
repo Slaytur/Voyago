9c7d14eb821942532b3a780db1fe9e97714eb081
Charlie Doan <90109397+charlesbae@users.noreply.github.com>
2024-11-02 01:07:43 -0500

    update: homepage stuff (sorry quentin)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,8 @@
     <div class="w-[500px] h-full  mt-8 ml-60">
         <div class="font-sourcesans text-white font-bold text-[80px] leading-[80px] tracking-tight">Plan the next era of your 
             <GradientText text="life" />
-            </div>
-        <div class="mt-8 mb-12 bg-white text-black font-bold pt-1 pb-1.5 flex flex-col justify-center items-center px-4 rounded-full w-fit"> <a href='/mainpage'>Explore ></a></div>
+        </div>
+        <p class="mt-8 leading-7 text-white text text-[18px] font-bold font-sourcesans">Revolutionizing ease of travel to every part of the world.</p>
+        <div class="mt-8 mb-12 bg-white text-black font-bold pt-1 pb-1.5 flex flex-col justify-center items-center px-4 rounded-full w-fit"> <a href='/signin'>Explore ></a></div>
     </div>
 </section>
