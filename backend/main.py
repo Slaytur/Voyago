b705af67d434422f30f9574d836a72de8d6817c6
bennlol <52867429+bennlol@users.noreply.github.com>
2024-11-02 15:41:47 -0500

    python backend

--- backend/main.py
+++ backend/main.py
@@ -1,99 +1,240 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from prompts import GET_CITIES
+from prompts import *
 from openai import OpenAI
 import dotenv
 import os
 import uvicorn
 import pickle
+import asyncio
+from ollama import Client
+
 dotenv.load_dotenv()
 
 API_KEY = os.environ.get("PERPLEXITY_API_KEY")
 client = OpenAI(api_key=API_KEY, base_url="https://api.perplexity.ai")
+ollama_client = Client(host='http://100.80.188.54:11434')
 
 app = FastAPI(title="python backend")
 
-class CityRequest(BaseModel):
+class DataRequest(BaseModel):
+    points_of_interest: list[str]
+    interest: list[str]
     location: str
-    interests: List[str]
-    date: str  # assuming ISO format 'YYYY-MM-DD'
-
-class ItineraryRequest(BaseModel):
-    cities: List[str]
-
-# Placeholder response model for cities
-class CityResponse(BaseModel):
-    cities: List[str]
-    descriptions: List[str]
-
-# Placeholder response model for itinerary
-class ItineraryResponse(BaseModel):
-    itinerary: dict
+    date: str
+    date_length: str
+    token: str
+    
+class DataResponse(BaseModel):
+    itinerary: str
+    weather: str
+    travel_tips: List[str]
+    packing_list: List[str]
 
 # Root route
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the Python Backend!"}
+@app.get("/", response_model=DataResponse)
+async def root(request: DataRequest):
+    if request.token != "i2JGyVfh3hVdzibdtx63sCnu3Nh4wDNDX3lCSWhkLwlH4wFr7jZQ6oq3wpb5StCR":
+        return {"error": "Invalid token"}
+    task1 = asyncio.create_task(get_travel_tips(request.points_of_interest))
+    task2 = asyncio.create_task(get_itinerary(request.interest, request.points_of_interest, request.location, request.date, request.date_length))
+    
+    weather = get_weather(request.points_of_interest, request.location, request.date)
+    packing_list = get_packing_list(request.points_of_interest, request.location, request.date, request.date_length)
+    travel_tips = await task1
+    itinerary = await task2
 
-# API endpoint to get recommended cities based on location, interests, and date
-@app.post("/get_cities", response_model=CityResponse)
-async def get_cities(request: CityRequest):
-    custom_prompt = GET_CITIES[::].replace("{location}", request.location).replace("{interests}", ", ".join(request.interests)).replace("{date}", request.date)
+    return DataResponse(itinerary=itinerary, weather=weather, travel_tips=travel_tips, packing_list=packing_list)
+    
+    
 
-    messages = [
-        {
-            "role": "system",
-            "content": "You are an expert trip planner with 20+ years of experience. You know all about the cities of the world."
-        },
-        {
+def get_weather(points_of_interest, location, date):
+    messages = [{
+        'role': 'user',
+        'content': GET_WEATHER[::].replace("{location}", ", ".join(location)).replace("{points_of_interest}", ", ".join(points_of_interest)).replace("{date}", date)
+    }]
+    response = ollama_client.chat(model='qwen2.5:14b-instruct-q4_K_M', messages=messages)['message']['content']
+    return response
+
+async def get_travel_tips(points_of_interest):
+    tips = []
+    
+    if os.path.exists("travel_tips_cache.pkl"):
+        with open("travel_tips_cache.pkl", "rb") as f:
+            tips = pickle.load(f)
+            return tips
+
+
+    custom_prompt = GET_TRAVEL_TIPS[::].replace("{points_of_interest}", ", ".join(points_of_interest))
+
+    messages = [{
             "role": "user",
             "content": custom_prompt
-        },
-    ]
+        },]
     
-    if os.path.exists("response_cache.pkl"):
-        with open("response_cache.pkl", "rb") as f:
-            response = pickle.load(f)
-    else:
-        response = client.chat.completions.create(
-            model="llama-3.1-sonar-large-128k-online",
-            messages=messages,
-        )
-        
-        with open('response_cache.pkl', 'wb') as f:
-            pickle.dump(response, f)
+    response = client.async_chat.completions.create(
+        model="llama-3.1-sonar-large-128k-online",
+        messages=messages,
+    )
     
-    cities = []
-    descriptions = []
 
     response_text = response.choices[0].message.content
-
     response_list = response_text.split("\n")
-    for i in range(len(response_list)):
-        if "#" in response_list[i]:
-            cities.append(response_list[i].replace("#", "").strip())
-            descriptions.append(response_list[i+1])
+    
+    tips = []
+    for line in response_list:
+        if '-' in line:
+            tips.append(line.replace("-", "").replace("*", "").strip())
+    
+    
+            
+    with open('travel_tips_cache.pkl', 'wb') as f:
+        pickle.dump(tips, f)
+
+    return tips
+
+def research_cities(cities, interests, location, date):
+    research = {}
+    
+    if os.path.exists("research_cities.pkl"):
+        with open("research_cities.pkl", "rb") as f:
+            research = pickle.load(f)
+            return research
+    os.exit()
+    attraction_types = ['restaurants', 'hotels', 'tourist attractions', 'niche and quaint attractions', 'local events']
+    non_food_interests = [x for x in interests if 'food' not in x and 'restaurant' not in x]
+    for city in cities:
+        research_dict = {}
+        for attraction_type in attraction_types:
+            custom_prompt = GET_CITY_ATTRACTION[::].replace("{location}", location).replace("{interests}", ", ".join(non_food_interests)).replace("{date}", date).replace("{city}", city).replace("{attraction_type}", attraction_type)
+
+            messages = [{
+                    "role": "user",
+                    "content": custom_prompt
+                },]
+            
+            response = client.chat.completions.create(
+                model="llama-3.1-sonar-large-128k-online",
+                messages=messages,
+            )
             
 
-    return CityResponse(cities=cities, descriptions=descriptions)
+            response_text = response.choices[0].message.content
+            response_list = response_text.split("\n")
+            
+            research_list = []
+            for i in range(len(response_list)):
+                if "#" in response_list[i]:
+                    research_list.append((response_list[i].replace("#", "").strip(), response_list[i+1]))
+            research_dict[attraction_type] = research_list
+            
+        research[city] = research_dict
+            
+    with open('research_cities.pkl', 'wb') as f:
+        pickle.dump(research, f)
+    
+    return research
 
-# API endpoint to get itinerary based on a list of cities
-@app.post("/get_itinerary", response_model=ItineraryResponse)
-async def get_itinerary(request: ItineraryRequest):
-    # Placeholder logic for itinerary creation
-    itinerary = {
-        "CityA": ["Visit museum", "Lunch at cafe"],
-        "CityB": ["City tour", "Dinner at local restaurant"],
-        "CityC": ["Hiking trail", "Visit historical site"]
-    }  # replace with actual logic based on cities in request
-    return ItineraryResponse(itinerary=itinerary)
+# research_dict = {'city': {'attraction_type': [(attraction_title, attraction_description), ...], ...}, ...}
 
-request = CityRequest(location="New York", interests=["history", "art"], date="2022-12-01")
+async def get_itinerary(interests, points_of_interest, location, date, length):
+    custom_prompt = GET_ITINERARY[::].replace("{interests}", ", ".join(interests)).replace("{date}", date).replace("{location}", location).replace("{points_of_interest}", ", ".join(points_of_interest)).replace("{length}", str(length))
+      
+    messages = [{
+        "role": "user",
+        "content": custom_prompt
+    },]
+    
+    if os.path.exists("get_itinerary.pkl"):
+        with open("get_itinerary.pkl", "rb") as f:
+            itin = pickle.load(f)
+            return itin
+    
+    response = client.async_chat.completions.create(
+        model="llama-3.1-sonar-huge-128k-online",
+        messages=messages,
+    )
+    
+    response_text = response.choices[0].message.content
+    with open('get_itinerary.pkl', 'wb') as f:
+        pickle.dump(response_text, f)
 
-print(get_cities(request))
+    
+    return response_text
+
+      
+def get_packing_list(points_of_interest, location, date, weather):
+    custom_prompt = GET_PACKING_LIST[::].replace("{points_of_interest}", ", ".join(points_of_interest)).replace("{location}", location).replace("{date}", date).replace("{weather}", weather)
+
+    messages = [{
+            "role": "user",
+            "content": custom_prompt
+        }]
+
+    response_text = ollama_client.chat(model='qwen2.5:14b-instruct-q4_K_M', messages=messages)['message']['content']
+    response_list = response_text.split("\n")
+    packing_list = []
+    for line in response_list:
+        if '-' in line:
+            packing_list.append(line.replace("-", "").replace("*", "").strip())
+
+    return packing_list
+    
+# API endpoint to get recommended cities based on location, interests, and date
+# @app.post("/get_cities", response_model=CityResponse)
+# async def get_cities(request: CityRequest, token:str):
+#     if token != "nxx5daGhBrFC2ZVKoDZ7dmCWoSGZarhP222exVfVdQ4PvEMb2iZ83T9KsbKyXDT6":
+#         return {"error": "Invalid token"}
+    
+#     custom_prompt = GET_CITIES[::].replace("{location}", request.location).replace("{interests}", ", ".join(request.interests)).replace("{date}", request.date)
+
+#     messages = [
+#         {
+#             "role": "system",
+#             "content": "You are an expert trip planner with 20+ years of experience. You know all about the cities of the world."
+#         },
+#         {
+#             "role": "user",
+#             "content": custom_prompt
+#         },
+#     ]
+    
+#     if os.path.exists("get_cities_cache.pkl"):
+#         with open("get_cities_cache.pkl", "rb") as f:
+#             response = pickle.load(f)
+#     else:
+#         response = client.chat.completions.create(
+#             model="llama-3.1-sonar-large-128k-online",
+#             messages=messages,
+#         )
+        
+#         with open('get_cities_cache.pkl', 'wb') as f:
+#             pickle.dump(response, f)
+    
+#     cities = []
+#     descriptions = []
+
+#     response_text = response.choices[0].message.content
+
+#     response_list = response_text.split("\n")
+#     for i in range(len(response_list)):
+#         if "#" in response_list[i]:
+#             cities.append(response_list[i].replace("#", "").strip())
+#             descriptions.append(response_list[i+1])
+            
+
+#     return CityResponse(cities=cities, descriptions=descriptions)
+
+
+    
 
 if __name__=="__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
-
+    # weather = get_weather(['louve', 'big ben'], 'France', '2024-12-01')
+    # print(weather)
+    # print(get_travel_tips(['louve', 'big ben']))
+    # print(get_packing_list(['louve', 'big ben'], 'France', '2024-12-01', weather))
+    # research = research_cities(['lille', 'Paris'], ['art','museums'], 'France', '2024-12-01')
+    # itinerary = get_itinerary(['art','museums'], ['louve', 'big ben'], 'France', '2024-12-01', '10')
 #pip freeze > requirements.txt
--- backend/prompts.py
+++ backend/prompts.py
@@ -1,1 +1,22 @@
+
+GET_WEATHER = "I am traveling to {points_of_interest} in {location}. I will be traveling on {date}, please estimate the weather for the area in this time of year. Do not give a weather forecast for each place, do it for the area as a whole. Include the usual chance of rain, temerature, and weather conditions. Only include the estimated weather, do not include anything else."
+
+GET_TRAVEL_TIPS = "Research travel guides for the attractions of {points_of_interest}. Then return a 20+ long bullet pointed list of every tip that you find. Combine all the tips into one big travel tips list. Make a mix of general tips and specific tips. Do not include places or things to do at the interests.\nFormat:\n - {travel_tip1}\n - {travel_tip2}\n - {travel_tip3}"
+
+GET_PACKING_LIST = "I am traveling to {location}. Create a packing list for {points_of_interest} and nearby attractions. The date of the visit is {date} and length is {length}. Then return a bullet pointed list of every item that could be needed.\nFormat:\n - {item1}\n - {item2}\n - {item3}"
+
+GET_ITINERARY= """
+Create a hyper-specific travel itinerary that is in the format of bullet points, separated by day.
+User wants to visit {location}.
+User wants to make sure to visit the {points_of_interest}
+User wants to see popular tourist destinations but also smaller niche quaint gems.
+User likes {interests}.
+User wants a packed schedule.
+User's vacation is on the {date} and is {length} days long.
+Be hyper-specific with recommendations, such as recommending specific restaurants, not just area of restaurant.""".strip()
+
+
+
+# unneeded
 GET_CITIES = "Think about all the cities in the country of (including) {location}. Then create a list of cities that are relatively close and align with these interests: {interests} around {date}. You should include a mix of highly popular cities, but also some smaller niche quaint gems. Only include titles with these cities, then under them have a description of the city and explain why it fits their interests.\nUse the following response schema:\n## {City}\n{description}"
+GET_CITY_ATTRACTION= "I am traveling to {location} on {date}. Create a list of {attraction_type} in {city} that align with the interests: {interests}. Only include titles with the name, then under the them include a description of the {attraction_type}. Do not include any other information.\nFormat:\n## {{attraction_type}}\n{desction}"
