b71ee3e65014b5c2368d16ba48396ff941742fda
Implycitt <qgbordelon@gmail.com>
2024-11-02 08:59:54 -0500

    feat: switch in header, page redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
             <GradientText text="life" />
         </div>
         <p class="mt-8 leading-7 text text-[18px] font-bold font-sourcesans">Revolutionizing ease of travel to every part of the world.</p>
-        <div class="mt-8 mb-12 bg-white text-black font-bold pt-1 pb-1.5 flex flex-col justify-center items-center px-4 rounded-full w-fit"> <a href='/signin'>Explore ></a></div>
+        <div class="mt-8 mb-12 bg-white text-black font-bold pt-1 pb-1.5 flex flex-col justify-center items-center px-4 rounded-full w-fit"> <a href='/dashboard'>Explore ></a></div>
         we should have a big splash image of beautyful nature (cut in like a bubbly shape if u know what I mean) with our accent/main color gradients behind it
     </div>
 </section>
