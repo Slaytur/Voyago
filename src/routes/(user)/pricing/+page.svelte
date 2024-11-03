6ac003e88ca28e77ba02fd9517c914bf09168fbc
Implycitt <qgbordelon@gmail.com>
2024-11-03 00:58:37 -0500

    feat: pricing page

--- src/routes/(user)/pricing/+page.svelte
+++ src/routes/(user)/pricing/+page.svelte
@@ -28,6 +28,20 @@
     #grad {
       padding: 0px 30px 0px; 
     }
+    .prices {
+      display: flex;
+      justify-content: center;
+      justify-items: center;
+      align-items: center;
+      padding-left: 30px;
+    }
+    .login-block {
+      text-align: center;
+      align-items: center;
+      text-align: center;
+      justify-content: center;
+      justify-items: center;
+    }
 </style>
 
 <section class="py-40 font-opensans">
@@ -41,17 +55,58 @@
       </div>
       <h2 class="text text-[26px] font-bold tracking-wide mb-10">Transparent pricing that fits your budget</h2>
     </section>
-    <section class="login black">
-        <div class="box flex flex-col items-start p-6">
-            <img src="" class="w-12 h-12" alt="StarterImg"/>
-            <h1 class=" mt-1.5">STARTER</h1>
-            <h1 class=" text-[50px]  -mt-2 font-bold">$0</h1>
-            <p class='mb-4'>
-                Your first itinerary is free!
-            </p>
-            <p>
-                You’re going to love it.
-            </p>
-        </div>
-    </section>
+    <div class="prices">
+      <section class="login black">
+          <div class="box flex flex-col items-start p-6">
+              <img src="" class="w-12 h-12" alt="StarterImg"/>
+              <h1 class=" mt-1.5">STARTER</h1>
+              <h1 class=" text-[50px]  -mt-2 font-bold">$0</h1>
+              <p class='mb-4'>
+                  Your first itinerary is free!
+              </p>
+              <p>
+                  You’re going to love it.
+              </p>
+          </div>
+      </section>
+      <section class="login black">
+          <div class="box flex flex-col items-start p-6">
+              <img src="" class="w-12 h-12"/>
+              <h1 class=" mt-1.5">EXPLORER</h1>
+              <h1 class=" text-[50px]  -mt-2 font-bold">$5</h1>
+              <p class='mb-4'>
+                  15 itineraries
+              </p>
+              <p>
+                  For the curious traveler, dipping their toes in adventure.
+              </p>
+          </div>
+      </section>
+      <section class="login black">
+          <div class="box flex flex-col items-start p-6">
+              <img src="" class="w-12 h-12"/>
+              <h1 class=" mt-1.5">VOYAGER</h1>
+              <h1 class=" text-[50px]  -mt-2 font-bold">$15</h1>
+              <p class='mb-4'>
+                  30 itineraries
+              </p>
+              <p>
+                  For the avid explorer, ready to roam further.
+              </p>
+          </div>
+      </section>
+      <section class="login black">
+          <div class="box flex flex-col items-start p-6">
+              <img src="" class="w-12 h-12"/>
+              <h1 class=" mt-1.5">VETERAN</h1>
+              <h1 class=" text-[50px]  -mt-2 font-bold">$30</h1>
+              <p class='mb-4'>
+                  60 itineraries
+              </p>
+              <p>
+                  For the seasoned adventurer, conquering every corner of the map.
+              </p>
+          </div>
+      </section>
+    </div>
 </section>
