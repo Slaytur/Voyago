88c210d490aaa692ce2673622b9bf065b4490599
Charlie Doan <90109397+charlesbae@users.noreply.github.com>
2024-11-03 11:18:29 -0600

    update: error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,8 +5,8 @@
 
 <section class="flex max-h-screen h-screen flex-col justify-center items-center font-sourcesans">
     <p class="text-[60px]">Sorry!</p>
-    <p use:concurrent={{ interval: 200 }} class="font-bold text-[50px]">
-        Error: {$page.status}
+    <p use:concurrent={{ interval: 120 }} class="font-bold text-[50px]">
+        Error {$page.status}: {$page.error?.message}
     </p>
     <!-- <p use:concurrent={{ interval: 60 }} class="font-bold text-[50px]">
         Sorry, the page you've requested has produced error: {$page.status}
