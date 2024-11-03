ef4aaa1bf94f12d292d53d8a771a0ce86891cfbe
bennlol <52867429+bennlol@users.noreply.github.com>
2024-11-03 08:36:14 -0600

    added filters for better loading

--- src/routes/(user)/dashboard/+page.server.ts
+++ src/routes/(user)/dashboard/+page.server.ts
@@ -13,7 +13,10 @@ export async function load (event): Promise<{ user: any, documents: any }> {
 
     // Fetch documents related to the user.
     const documents = await dbaccount.listDocuments("6726c103000d53b938ab", "6726c10f0033575af875", [
-        // Query.equal("userId", locals.user.$id)
+        // Query.equal("userId", locals.user.$id),
+        Query.orderDesc("createdAt"),
+        Query.limit(10)
+
     ]);
     console.log(documents.documents);
     // Pass the user and documents to the page.
