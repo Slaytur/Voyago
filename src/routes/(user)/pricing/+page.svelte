da94150981061bac3c49a7e290d49aa289a8f1d3
Implycitt <qgbordelon@gmail.com>
2024-11-02 23:57:37 -0500

    pricing

--- src/routes/(user)/pricing/+page.svelte
+++ src/routes/(user)/pricing/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Button } from "bits-ui";
+    import GradientText from "$lib/components/GradientText.svelte";
 </script>
 
 <style>
@@ -11,38 +12,39 @@
         justify-items: center;
         padding-top: 1vi;
     }
+    .box {
+      border: 5px solid black;
+      border-radius: 30px;
+      width: 300px;
+      height: 300px;
+    }
+    .text {
+      display: flex;
+      align-items: center;
+      text-align: center;
+      justify-content: center;
+      justify-items: center;
+      padding-bottom: 30px;
+      font-size: 50px;
+    }
+    #grad {
+      padding: 0px 30px 0px; 
+    }
 </style>
 
 <section class="py-48">
+    <section class="top">
+      <div class="text">
+        World-class 
+        <div id="grad" class="text">
+            <GradientText interval={210} text="adventures" />
+        </div>
+          within reach
+      </div>
+    </section>
     <section class="login">
-        <div class="flex flex-row space-x-10 font-sourcesans tracking-wide">
-            <div class="flex flex-col justify-between items-center outline outline-4 outline-[#152aa5] rounded-lg h-96 font-bold text-xl">
-                <div class="space-y-4">
-                    <h1 class="flex flex-col justify-center items-center w-full h-20 rounded-t-lg bg-gradient-to-br from-[#4f52ff] via-[#152aa5] to-[#0f29b9] p-8">Voyago Explorer</h1>
-                    <h2>5 itineraries for $5</h2>
-                </div>
-                <Button.Root class="inline-flex h-12 items-center justify-center rounded-input rounded-xl outline outline-white text-[15px] max-w-20 w-20 font-semibold text-white er:bg-dark/95 active:transition-all bg-gradient-to-br from-[#4f52ff] via-[#152aa5] to-[#0f29b9]">
-                    $5
-                </Button.Root>
-            </div>
-            <div class="flex flex-col justify-between items-center outline outline-4 outline-[#04cf41] rounded-lg h-96 font-bold text-xl">
-                <div class="space-y-4">
-                    <h1 class="flex flex-col justify-center items-center w-full h-20 rounded-t-lg bg-gradient-to-br from-[#52ffa9] via-[#04cf41] to-[#05e886] p-8">Voyago Voyager</h1>
-                    <h2>15 itineraries for $10</h2>
-                </div>
-                <Button.Root class="inline-flex h-12 items-center justify-center rounded-input rounded-xl outline outline-white text-[15px] max-w-20 w-20 font-semibold text-white er:bg-dark/95 active:transition-all bg-gradient-to-br from-[#52ffa9] via-[#04cf41] to-[#05e886]">
-                    $10
-                </Button.Root>
-            </div>
-            <div class="flex flex-col justify-between items-center outline outline-4 outline-[#ff3c00] rounded-lg h-96 font-bold text-xl">
-                <div class="space-y-4">
-                    <h1 class="flex flex-col justify-center items-center w-full h-20 rounded-t-lg bg-gradient-to-br from-[#ff734c] via-[#ff3c00] to-[#ff5e00] p-8">Voyago Veteran</h1>
-                    <h2>50 itineraries for $30</h2>
-                </div>
-                <Button.Root class="inline-flex h-12 items-center justify-center rounded-input rounded-xl outline outline-white text-[15px] max-w-20 w-20 font-semibold text-white er:bg-dark/95 active:transition-all bg-gradient-to-br from-[#ff734c] via-[#ff3c00] to-[#ff5e00]">
-                    $30
-                </Button.Root>
-            </div>
+        <div class="box">
+          yes 
         </div>
     </section>
 </section>
